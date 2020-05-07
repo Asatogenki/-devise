8ef8f66f349b96475db7924b47c1ddac487cebc8
Asatogenki <asa.326.gen@gmail.com>
2020-05-07 21:47:18 +0900

    user周り削除

--- config/routes.rb
+++ config/routes.rb
@@ -16,13 +16,13 @@ Rails.application.routes.draw do
   # get 'users/:id' => 'users#show'
 
 #postsのルーティング
-  get 'posts/index' => "posts#index"
-  get 'posts/new' => "posts#new"
-  get 'posts/:id' => "posts#show"
-  post 'posts/create' => "posts#create"
-  get 'posts/:id/edit' => "posts#edit"
-  post 'posts/:id/update' => "posts#update"
-  post 'posts/:id/destroy' => "posts#destroy"
+  # get 'posts/index' => "posts#index"
+  # get 'posts/new' => "posts#new"
+  # get 'posts/:id' => "posts#show"
+  # post 'posts/create' => "posts#create"
+  # get 'posts/:id/edit' => "posts#edit"
+  # post 'posts/:id/update' => "posts#update"
+  # post 'posts/:id/destroy' => "posts#destroy"
 #homeのルーティング  
   get '/' => "home#top"
   get 'about' => "home#about"
