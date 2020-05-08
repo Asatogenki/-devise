39016aa92c337055030a7485ffd2cf39f3ae1197
Asatogenki <asa.326.gen@gmail.com>
2020-05-08 21:53:21 +0900

    修正

--- db/migrate/20200422065025_create_posts.rb
+++ db/migrate/20200422065025_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
-    create_table :posts do |t|
-      t.text :content
+    # create_table :posts do |t|
+    #   t.text :content
 
-      t.timestamps
+    #   t.timestamps
     end
   end
 end
